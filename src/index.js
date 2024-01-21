fbea7e718cc980b4a4f33420c59ffbea8dffa921
Yuliia <yulja01@gmail.com>
2024-01-21 12:57:43 -0600

    added atatus for monitoring

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('746864feeeb64ef597be8dd5b8f96f6b').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
